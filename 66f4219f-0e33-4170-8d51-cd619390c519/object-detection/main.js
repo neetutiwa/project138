33ef17d8e9dd1fe1e9fad83534fb0d1862bf80e3
neetutiwa <110733241+neetutiwa@users.noreply.github.com>
2023-03-26 12:44:23 +0530

    Add files via upload

---
+++ 66f4219f-0e33-4170-8d51-cd619390c519/object-detection/main.js
@@ -0,0 +1,48 @@
+img = "";
+status ="";
+objects = [];
+function preload() {
+    img=loadImage('dog_cat.jpg');
+}
+
+function setup() {
+canvas = createCanvas(640,420);
+canvas.center();
+objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img,gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw() {
+    image(img,0,0,640,420);
+
+     if(status !="")
+     {
+     }
+
+    Fill("#FF0000");
+    text("Dog",45,75);
+    noFill();
+    stroke("#FF0000");
+    rect(30,60,450,350);
+
+    image(img,0,0,640,420);
+    Fill("#FF0000");
+    text("Cat",320,120);
+    noFill();
+    stroke("#FF0000");
+    rect(300,90,270,320);
+}
